0ca3a36f9b3a49c60c2148da7a0cd8248e35a133
Ramez Taher <ramezgamer11@gmail.com>
2022-10-04 16:54:19 +0100

    fixing cors problem

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import path from 'path'
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import morgan from 'morgan'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import path from "path"
+import express from "express"
+import dotenv from "dotenv"
+import colors from "colors"
+import morgan from "morgan"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import connectDB from "./config/db.js"
 
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import uploadRoutes from './routes/uploadRoutes.js'
+import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+import orderRoutes from "./routes/orderRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
 
 dotenv.config()
 
@@ -17,33 +17,38 @@ connectDB()
 
 const app = express()
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  next()
+})
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"))
 }
 
 app.use(express.json())
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes)
+app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
 
-app.get('/api/config/paypal', (req, res) =>
+app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   )
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
+  app.get("/", (req, res) => {
+    res.send("API is running....")
   })
 }
 
