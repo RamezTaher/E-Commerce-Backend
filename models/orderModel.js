491f3f6d5b4c499812f6c482dbf306cf1f121440
Ramez Taher <ramezgamer11@gmail.com>
2022-10-14 22:09:07 +0100

    matching models

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,29 +1,29 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 
 const orderSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'User',
+      ref: "User",
     },
     orderItems: [
       {
         name: { type: String, required: true },
-        qty: { type: Number, required: true },
+        quantity: { type: Number, required: true },
         image: { type: String, required: true },
         price: { type: Number, required: true },
         product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
-          ref: 'Product',
+          ref: "Product",
         },
       },
     ],
     shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
-      postalCode: { type: String, required: true },
+      zipCode: { type: String, required: true },
       country: { type: String, required: true },
     },
     paymentMethod: {
@@ -73,6 +73,6 @@ const orderSchema = mongoose.Schema(
   }
 )
 
-const Order = mongoose.model('Order', orderSchema)
+const Order = mongoose.model("Order", orderSchema)
 
 export default Order
